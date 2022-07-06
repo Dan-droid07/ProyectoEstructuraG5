cfcbdbe96bf14ef1b23c984b02a8cf0349425fae
dsiles <dsiles@IT-CR-ZF-0264>
2022-07-06 15:24:14 -0600

    Aprendiendo sobre como usar Github, parte investigativa.

--- src/proyectogrupo5/ProyectoGrupo5.java
+++ src/proyectogrupo5/ProyectoGrupo5.java
@@ -14,6 +14,7 @@ public class ProyectoGrupo5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Aprendiendo sobre como usar GITHUB, parte investigativa");
         // TODO code application logic here
     }
     
