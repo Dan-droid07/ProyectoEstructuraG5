6b26201c2590d1f374f025983ea84b8b20dd8634
Hans Mayorga <108765552+Mayor008@users.noreply.github.com>
2022-07-08 12:40:19 -0600

    Add files via upload

---
+++ Avance Proyecto/src/AvanceProyecto_1/RecibirSobre.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package AvanceProyecto_1;
+
+
+public class RecibirSobre {
+
+    
+   public static void main(String[] args) {
+
+        HeroesCustomLinkedList heroesList = new HeroesCustomLinkedList();
+
+        Heroes hero1 = new Heroes("Han Solo", 1);
+        Heroes hero2 = new Heroes("Han Solo", 2);
+        Heroes hero3 = new Heroes("Han Solo", 3);
+        Heroes hero4 = new Heroes("Han Solo", 4);
+        Heroes hero5 = new Heroes("Han Solo", 5);
+
+        heroesList.insert(hero1);
+        heroesList.insert(hero2);
+        heroesList.insert(hero3);
+        heroesList.insert(hero4);
+        heroesList.insert(hero5);
+        
+    }
+   
+}
+
